ca4da8d4248c3fcc994a4db1b0bc26a7e5e41361
Mohamed <madiounimohamed92@gmail.com>
2020-10-12 14:23:42 +0100

    fix history

--- client/src/outils/history.js
+++ client/src/outils/history.js
@@ -6,8 +6,12 @@
 
     if (hist <= 240) return `${Math.ceil(hist/60)} mins ago`;
     else if (hist <= 86400) return `${Math.ceil(hist / 3600)} hours ago`;
-    else {
+    else if(hist<604800){
       let day = Math.floor(hist / 86400);
       return `${day} days ago`;
+    }
+    else{
+      let week=Math.floor(hist / 604800)
+      return `${week} week ago`;
     }
   }
