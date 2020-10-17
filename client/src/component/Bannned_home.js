cdca4a709dca01e6a08def6a5bef59bd8ef8ead7
KarimNaouar <KarimNaouar@users.noreply.github.com>
2020-10-17 22:09:38 +0200

    Clean the error

--- client/src/component/Bannned_home.js
+++ client/src/component/Bannned_home.js
@@ -1,6 +1,16 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import { GET_ERRORS } from "../actions/types";
+import { useDispatch } from "react-redux";
 function Bannned_home() {
+    const dispatch = useDispatch();
+   
+    useEffect(()=>{
+        dispatch({
+          type: GET_ERRORS,
+          payload: {},
+        });
+      },[])
+
     return (
         <div>
             <h1>helooooo</h1>
