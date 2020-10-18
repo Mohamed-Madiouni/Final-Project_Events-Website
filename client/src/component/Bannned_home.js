7c60bb5373f6829558006478af8a4b0c18d6ed52
KarimNaouar <KarimNaouar@users.noreply.github.com>
2020-10-18 20:17:27 +0200

    Add ban page

--- client/src/component/Bannned_home.js
+++ client/src/component/Bannned_home.js
@@ -1,21 +1,40 @@
 import React, { useEffect } from "react";
 import { GET_ERRORS } from "../actions/types";
 import { useDispatch } from "react-redux";
-function Bannned_home() {
-    const dispatch = useDispatch();
-   
-    useEffect(()=>{
-        dispatch({
-          type: GET_ERRORS,
-          payload: {},
-        });
-      },[])
+import "../Bannned_home.css";
 
-    return (
-        <div>
-            <h1>helooooo</h1>
+function Bannned_home() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({
+      type: GET_ERRORS,
+      payload: {},
+    });
+  }, []);
+
+  return (
+    <div class="ban">
+      <div class="scene">
+        <div class="overlay"></div>
+        <div class="overlay"></div>
+        <div class="overlay"></div>
+        <div class="overlay"></div>
+        <span class="bg-403">403</span>
+        <div class="text">
+          <span class="hero-text"></span>
+          <span class="msg">
+            Your <span>account</span> get baned.
+          </span>
+          <span class="support">
+            <span>unexpected?</span>
+            <a href="#">contact support</a>
+          </span>
         </div>
-    )
+        <div class="lock"></div>
+      </div>
+    </div>
+  );
 }
 
-export default Bannned_home
+export default Bannned_home;
