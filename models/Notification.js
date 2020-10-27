dd0beae8fbbb85170ed960498f4b165df4681716
KarimNaouar <KarimNaouar@users.noreply.github.com>
2020-10-27 13:27:23 +0100

    Initial implementation for notification

---
+++ models/Notification.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+const EventSchema = new schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  state: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  counter: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+module.exports = mongoose.model("notification", EventSchema);
