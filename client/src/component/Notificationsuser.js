4b8a9e68ad82fdda817f390b9eb5a0184dd1e217
KarimNaouar <KarimNaouar@users.noreply.github.com>
2020-11-03 22:51:17 +0100

    Notification center rework

--- client/src/component/Notificationsuser.js
+++ client/src/component/Notificationsuser.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import historyuser from "../outils/history";
 import notif, { filter_notif } from "../outils/notif_length";
 import { getUsers } from "../actions/adminaction";
+import "../notifications.css";
 function Notificationsuser() {
   const dispatch = useDispatch();
   const [countnotif, setCountnotif] = useState(0);
@@ -26,19 +27,23 @@ function Notificationsuser() {
        <>
       {users.length!=0&&<div id="modalnotifuser" style={{ padding: 0, margin:0 }}>
 
-            <div style={{ padding: 10 }}>
-              <h4 className="center">Notifications Center</h4>
+      <div >
+              <h4 className="center" style={{ marginTop: "20px" }}>Notifications Center</h4>
             </div>
-              <table>
-                <thead>
-                  <tr>
-                    <th className="center-align">Title</th>
-                    <th className="center-align">Content</th>
-                    <th className="center-align">Executed by</th>
-                    <th className="center-align">Created at</th>
-                  </tr>
-                </thead>
-                <tbody>
+
+            <div className="notification-page__content" style={{ marginBottom: "0px" }}>
+              <div className="notification-page__content__title">
+                <span>Notifications</span>
+                </div>
+                <div className="notification-page__content__container">
+                  <div className="notification-container">
+                    <div className="notification-per-period">
+                      <div className="notification-per-period__title">
+                        <div className="x-flex-column-h-center-v-any ">
+                          <span>2020-10-06</span>
+                          </div>
+                          </div>
+                          <div className="notification-per-period__link">
                   {(notifsize>0) &&
                     (filter_notif(allnotif,auth.user._id))
                       .slice(0)
@@ -46,59 +51,47 @@ function Notificationsuser() {
                       .slice(0, 10 + countnotif * 10)
                       .map((el) => {
                         return (
-                          <tr key={el._id} className="center-align" style={{ margin: "10px" }}>
-                            <td className="center-align" style={{ padding: 10 }}>
-                              <span>{el.title}</span>
-                            </td>
-                            <td className="center-align" style={{ padding: 10 }}>
-                              <span className="center-align">{el.content}</span>
-                            </td>
-                            <td className="center-align" style={{ padding: 10 }}>
-                            <span>{"The " +el.role + ": "} {users.find(e=>e._id==el.userId).fname + " "}{users.find(e=>e._id==el.userId).lname}</span> 
-                            </td>
-                            <td className="center-align" style={{ padding: 10 }}>
-                              <span>
-                                <i
-                                  className=" tiny material-icons"
-                                  style={{
-                                    transform: "translateY(2px)",
-                                    marginRight: 5,
-                                  }}
-                                >
-                                  history
-                                </i>
-                                {historyuser(el.created_at)}
-                              </span>
-                            </td>
-                          </tr>
-                        );
-                      })}
-                </tbody>
-              </table>
-               <p/>
-                {(countnotif + 1) * 10 < notifsize && (
-                  <div
-                    style={{
-                      position: "abosolute",
-                      background: "cadetblue",
-                      cursor: "pointer",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      color: "black"
-                    }}
-                    id="loadMore"
-                    className="thb-gp-load-more"
-                    data-thb-gp-lm-type="event"
-                    data-org-text="MORE"
-                    onClick={() => {
-                      setCountnotif(countnotif + 1);
-                    }}
-                  >
-                    SHOW MORE
-                  </div>
-                )}
-        </div>}
+                          <div key={el._id} className="notification-per-period__period-card">
+                          <div className="x-flex-column-h-center-v-any" style={{minWidth: "90px"}}>
+                            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAQlBMVEVHcEwun+0un+0un+0un+0un+0un+0un+0un+0un+0un+0un+3+//9LrfCx2/h8w/Ti8v04pO5TsPDI5vrF5Pqu2vjMOoLpAAAAC3RSTlMAichYCprR+CTqMrIcawcAAADYSURBVDjLhVNZFoQgDGNHZHO9/1VHrHYA9bVfhYQmQMsYhpJaOGud0FKxZwzaegyrhw5WvIJPCm+qDMY/wlRFRudfwo14/hU/GFcNZfxHGPDB670Y6xU/BSr/MYWQKootIrqFW4o+HNjudFPFKiYhm6vtkzJDLm+FJYQVK8c1hOXWEA9/jYZgrvUYu4s4dnmMWwYKwHm7OBYJU5gKZd8LfCyQgBIZKFUCEgLlAGngYhKvmaA2KKX/NbuHyrl/KPKpyc+iv5tsGLrlyKal254eHHr06OH9Hv8fr7ckO+3gY/cAAAAASUVORK5CYII=" alt="Notification" />
+                              </div>
+                              <div>
+                                <div className="notification-per-period__period-card__type">
+                                  <span>{el.title}</span>
+                                  </div>
+                                  <div className="notification-per-period__period-card__content">{el.content}</div>
+                                  <div className="notification-per-period__period-card__date">{historyuser(el.created_at)}</div>
+                                  </div></div>
+                    )})}
+                                  </div>
+                                  <p/>
+            {(countnotif + 1) * 10 < allnotif.length && (
+              <div
+                style={{
+                  position: "abosolute",
+                  background: "cadetblue",
+                  cursor: "pointer",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  color: "black"
+                }}
+                id="loadMore"
+                className="thb-gp-load-more"
+                data-thb-gp-lm-type="event"
+                data-org-text="MORE"
+                onClick={() => {
+                  setCountnotif(countnotif + 1);
+                }}
+              >
+                SHOW MORE
+              </div>
+            )}
+         </div>
+         </div>
+         </div>
+         </div>
+    </div>} 
 
         </>
 
