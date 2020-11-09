d547134f17117f7441d3c270dbe268c43b650fec
KarimNaouar <KarimNaouar@users.noreply.github.com>
2020-11-09 23:00:50 +0100

    Add  links and some style to the notification center

--- client/src/component/Notificationsuser.js
+++ client/src/component/Notificationsuser.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { GET_ERRORS } from "../actions/types";
 import { useSelector, useDispatch } from "react-redux";
 import historyuser from "../outils/history";
@@ -11,6 +12,7 @@ function Notificationsuser() {
   const allnotif = useSelector((state) => state.notification.notifications);
   let auth = useSelector((state) => state.auth);
   var notifsize=notif(allnotif,auth.user._id);
+  const history = useHistory()
   const users=useSelector(state=>state.admin.users)
   useEffect(() => {
     dispatch({
@@ -67,7 +69,29 @@ function Notificationsuser() {
                         {
                             
                             el.map((el,i)=>
-                            <div key={i} className="notification-per-period__period-card">
+                            <div key={i} style={{cursor:"pointer"}} className="notification-per-period__period-card" onClick={() => {
+                              if (el.notiftype=="Event_Validation") { history.push("/events/"+el.compid)}
+                              else if (el.notiftype=="New_Event") { history.push("/events/")}
+                              else if (el.notiftype=="Event_Edition") {history.push("/events/"+el.compid)}
+                              else if (el.notiftype=="Comment_Reply_organizer") { history.push("/events/"+el.compid)}
+                              else if (el.notiftype=="Comment_Reply_User") { history.push("/events/"+el.compid)}
+                              else if (el.notiftype=="New_Follow") { history.push("/dasboard")}
+                              else if (el.notiftype=="New_Like") { history.push("/events/"+el.compid)}
+                              else if (el.notiftype=="New_Dislike") { history.push("/events/"+el.compid)}
+                              else if (el.notiftype=="Remove_Follow") { history.push("/")}
+                              else if (el.notiftype=="Event_Deleted") { history.push("/")}
+                              else if (el.notiftype=="Event_Invalidation") { history.push("/dashboard/")}
+                              else if (el.notiftype=="New_Participation") { history.push("/events/"+el.compid)}
+                              else if (el.notiftype=="Cancel_Participation") { history.push("/events/"+el.compid)}
+                              else if (el.notiftype=="Event_Closed") { history.push("/dashboard/")}
+                              else if (el.notiftype=="Event_Opened") { history.push("/events/"+el.compid)}
+                              else if (el.notiftype=="Account_Banned") {history.push("/dashboard")}
+                              else if (el.notiftype=="Account_Unbanned") { history.push("/dashboard")}
+                              else if (el.notiftype=="Account_Alerted") { history.push("/dashboard")}
+                              else if (el.notiftype=="Alert_Removed") { history.push("/dashboard")}
+                              else if (el.notiftype=="New_Comment") { history.push("/events/"+el.compid)}
+                              else {  history.push("/")}
+                              }}>
                           <div className="x-flex-column-h-center-v-any" style={{minWidth: "90px"}}>
 
                         <img src={users.find(e=>e._id==el.userId).avatar} alt="" className="circle"  style={{ marginRight: "8px",width:"40px" ,height:"40px"}}/>
