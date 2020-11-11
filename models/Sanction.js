6f33a89995ada4b346b5e2e8ae9ac4ca7ee1ffc6
KarimNaouar <KarimNaouar@users.noreply.github.com>
2020-11-11 21:03:41 +0100

    Add the new sanction Schema

---
+++ models/Sanction.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  type: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  reason: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("saction", userSchema);
