f5a863ac8fda0b3cb1652de188b619d98144d86c
mohamed madiouni <madiounimohamed92@gmail.com>
2021-02-05 14:03:53 +0100

    update

--- client/src/outils/resize.js
+++ client/src/outils/resize.js
@@ -1,6 +1,6 @@
 export default function resize (url){
     let t=url.split("/")
-    t.splice(6,0,"w_0.8,h_0.8,c_scale")
+    t.splice(6,0,"w_0.9,h_0.9,c_scale")
     return t.join("/")
     }
     
